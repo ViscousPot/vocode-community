3cd55860a1f5b6ddfd35252bf75d8981cb41d4b8
ViscousPot <viscouspotential@gmail.com>
2025-07-03 22:18:31 +0100

    Update plugin.lua

--- core_plugins/text_injector/plugin.lua
+++ core_plugins/text_injector/plugin.lua
@@ -1,5 +1,15 @@
 function applyTemplate(data, template)
-  return string.interpolate(template, { text = data["text"], date = os.date("yyyy-MM-dd", tonumber(data["timestamp"])), time = os.date("HH:mm:ss", tonumber(data["timestamp"])) })
+  print("/////applyTemplate")
+  local text = data["text"]
+  print(text)
+  local timestamp = data["timestamp"]
+  print(timestamp)
+  local timestampNum = toNumber(timestamp)
+  print(timestampNum)
+  local dateString = os.date("yyyy-MM-dd", timestampNum)
+  local timeString = os.date("HH:mm:ss", timestampNum)
+  print(string.interpolate(template, { text = text, date = dateString, time = timeString }))
+  return string.interpolate(template, { text = text, date = dateString, time = timeString })
 end
 
 function writeAtOffsetToFile(settings, data, editOffset)
@@ -36,10 +46,10 @@ function add(settings, data)
     return false
   end
   
-  local editOffset = tonumber(settings["Edit Offset"]) or 0
+  local editOffset = toNumber(settings["Edit Offset"]) or 0
 
   local position = 0
-  file.setPosition(0);
+  file.setPosition(0)
 
   if string.len(searchString) < 1 then
     if editOffset < 0 then
@@ -55,7 +65,7 @@ function add(settings, data)
 
   while file.getPosition() < file.getLength() do
     position = file.getPosition()
-    local line = file.readForwardLine();
+    local line = file.readForwardLine()
 
     if (string.find(line, searchString)) then 
 
@@ -79,32 +89,43 @@ function remove(settings, data)
     return false
   end
   local position = 0
-  file.setPosition(0);
+  file.setPosition(0)
 
   local originalText = applyTemplate(data, settings["Formatting"])
 
+  print(originalText)
+
   while file.getPosition() < file.getLength() do
     position = file.getPosition()
-    local line = file.readForwardLine();
+    local line = file.readForwardLine()
+    print(line)
 
     local firstLine = string.match(originalText, "([^\n]*)")
     local count = 0
-    for _ in string.gmatch(originalText, "[^\n]*") do
-        count = count + 1
+    for _ in string.gmatch(originalText, "([^\n]+)") do
+      count = count + 1
     end
+
+    print(count)
     if (line == firstLine) then 
-      for i = 1,count  do
-        file.readForwardLine()
-      end 
+      if (count > 1) then
+        for i = 1,count-1  do
+          print(file.readForwardLine())
+        end 
+      end
       local endOfLinePosition = file.getPosition()
 
       local fileLength = file.getLength()
 
       local remainingBytes = file.read(fileLength - endOfLinePosition)
+
+      print(remainingBytes)
+
       file.setPosition(position)
 
       file.writeString(remainingBytes)
-      file.truncate(fileLength - string.len(originalText .. '\n'))
+
+      file.truncate(toInteger(math.max(fileLength - string.len(originalText .. '\n'), 0)))
               
       file.close("applyTemplate", data)
       return true
@@ -112,7 +133,7 @@ function remove(settings, data)
   end
 
   file.close("applyTemplate", data)
-  return true
+  return false
 end
 
 function getInitialSettings()
