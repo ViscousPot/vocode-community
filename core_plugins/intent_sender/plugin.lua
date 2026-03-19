f37bf0764f165b2f3361d1c541a19d808ff9daf4
ViscousPot <viscouspotential@gmail.com>
2026-03-19 21:52:25 +0000

    Update plugin.lua

--- core_plugins/intent_sender/plugin.lua
+++ core_plugins/intent_sender/plugin.lua
@@ -14,5 +14,6 @@ function getInitialSettings()
         { name = "Package", _description = "The package name of the target app that will receive the intent." , type = "text", _hint = "com.example.app" },
         { name = "Component", _description = "The class name of the activity or service within the target app that should handle the intent.", type = "text", _hint = "com.example.app.TargetActivity" },
         { name = "Action", _description = "The action to be used in the intent." , type = "text", _hint = "com.example.app.ACTION_SEND_TEXT" },
+        { name = "Ignore Errors", _description = "When enabled, errors will be logged but will not cause the plugin to be disabled.", type = "checkbox", _default = false },
     }
 end
